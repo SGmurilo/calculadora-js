73fab81bb82ba49dfcdddc7c12396f9f78489b6a
SGmurilo <murilo.sg02@gmail.com>
2022-12-08 19:30:02 -0300

    calculadora windows

--- estudos/calculadora/script.js
+++ estudos/calculadora/script.js
@@ -1,5 +1,21 @@
 
+const prevOp = document.querySelector("#prev-op");
+const currentOp = document.querySelector("#current-op");
+const buttons = document.querySelectorAll("#btn-op button");
 
+class Calculadora {
+    
+}
 
-
-
+buttons.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        const value = e.target.innerText;
+        
+        if(+value >= 0 || value ==="."){
+            console.log("num "+value)
+        }
+        else{
+            console.log("op "+value)
+        }
+    });
+});
