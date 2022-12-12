e9cda36704244830d562bb7b05e7220192596e48
SGmurilo <murilo.sg02@gmail.com>
2022-12-12 20:54:23 -0300

    calculadora

--- estudos/calculadora/script.js
+++ estudos/calculadora/script.js
@@ -3,19 +3,63 @@ const prevOp = document.querySelector("#prev-op");
 const currentOp = document.querySelector("#current-op");
 const buttons = document.querySelectorAll("#btn-op button");
 
-class Calculadora {
+class Calculator {
+    constructor(prevOp, currentOp){
+        this.prevOp = prevOp;
+        this.currentOp = currentOp;
+        this.currentOperation = "";
+    }
+
+    // add digit to update calalculator screen
+    addDigit(digit){
+        if(digit === "." && currentOp.innerText.includes(".")){
+            return;
+        }
+        this.currentOperation = digit;
+        this.updateScreen();
+    }
+
+
+    processOperation(digit){
+        
+        let operationValue;
+
+        let previous = +this.prevOp.innerText;
+        let current = +this.currentOp.innerText;
+
+        switch(operation){
+            case "+":
+            operationValue = previous + current;
+            this.updateScreen(operationValue, operation, current, previous )
+            break;
+            default:
+                return;
+        }
+    }
+    updateScreen(operationValue = null, operation = null, current = null, previous = null){
+        this.currentOp.innerText += this.currentOperation;
+
+
+
+    }
+
+
+
+
     
 }
+
+const calculator = new Calculator(prevOp,currentOp);
 
 buttons.forEach((btn) => {
     btn.addEventListener("click", (e) => {
         const value = e.target.innerText;
         
         if(+value >= 0 || value ==="."){
-            console.log("num "+value)
+            calculator.addDigit(value)
         }
         else{
-            console.log("op "+value)
+            calculator.processOperation(value)
         }
     });
 });
